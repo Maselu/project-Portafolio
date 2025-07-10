e08d7adfc72efd0a10ebab944c55879c1e02b2d2
Mamadu <mamadudembele@gmail.com>
2025-07-10 16:42:19 +0200

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -22,6 +23,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
