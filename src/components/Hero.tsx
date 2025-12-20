400086955d244b9bf425e444c30c9170cc3fd871
Mamadu <mamadudembele@gmail.com>
2025-12-20 22:02:21 +0100

    feat: add CV download

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,7 +69,7 @@ const Hero = () => {
             </motion.button>
 
             <motion.a
-              href="/public/zRecursosIMG/cv.pdf"
+              href="/cv.pdf"
               download="CV_Mamadou_Cellou.pdf"
               target="_blank"
               rel="noopener noreferrer"
